4dc2e265c410ed70b1e6dc4fc46c7247742886bd
jsaddiction <04reduramax@gmail.com>
2020-12-22 12:40:22 -0500

    add permission settings

--- Install_TrailerTech.sh
+++ Install_TrailerTech.sh
@@ -2,7 +2,11 @@
 
 #### USER SETTINGS
 # installDirectory: can not be a root directory default: /config/scripts/TrailerTech
+# PUID is the id of the user who will be calling this script. Uncomment and set to appropriate values if KodiLibrarian files have wrong uid set
+# PGID same as PUID but for group
 installDirectory="/scripts/TrailerTech"
+# ${PUID}=1234
+# #{PGID}=1234
 
 ### Scripts variables
 parentDir="$(dirname ${installDirectory})"
@@ -24,6 +28,7 @@ if [ ! -d ${parentDir} ]
 then
     echo "*********** Creating script parent directory ************"
     error=$(mkdir -p ${parentDir} 2>&1) || { echo "Failed to create script parent directory ${parentDir}. ERROR: ${error}" ; exit 1; }
+    error=$(chmod -R ug+rw ${parentDir} 2>&1) || { echo "Failed to set permissions on ${parentDir}. ERROR: ${error}" ; exit 1; }
 fi
 
 ### Clone TrailerTech or update if already exists
@@ -35,6 +40,11 @@ else
     echo "********** UPDATING TrailerTech **********"
     error=$(git -C ${installDirectory} pull 2>&1) || { echo "Failed to update local repo. ERROR: ${error}" ; }
 fi
+
+### Set permissions and install dependancies
+echo "********** Setting Permissions for TrailerTechnician **********"
+error=$(chown -R ${PUID}:${PGID} ${installDirectory} 2>&1) || { echo "Failed to set ownership on ${installDirectory}. ERROR: ${error}" ; }
+error=$(chmod -R ug+rwx ${installDirectory} 2>&1) || { echo "Failed to set permissions on ${installDirectory}. ERROR: ${error}" ; exit 1;}
 
 echo "********** Installing TrailerTech Dependencies **********"
 error=$(python3 -m pip install -r ${requirementsFile} 2>&1) || { echo "Failed to install dependencies. ERROR: ${error}" ; exit 1; }
