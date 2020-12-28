b17263991357fb39d336b242ef2e2aca482061dd
jsaddiction <04reduramax@gmail.com>
2020-12-27 19:33:28 -0500

    log trailer found

--- TrailerTech.py
+++ TrailerTech.py
@@ -55,7 +55,7 @@ class TrailerTech():
         self.directoriesScanned += 1
         
         if folder.hasTrailer:
-            log.debug('Skipping. Local trailer found: {}'.format(folder.trailer.path))
+            log.info('Skipping. Local trailer found: {}'.format(folder.trailer.path))
             self.trailersFound += 1
             return
 
